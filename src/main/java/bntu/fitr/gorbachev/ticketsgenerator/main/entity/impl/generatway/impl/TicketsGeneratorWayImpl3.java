06201a3798ec47f652d6f7dbb6793cf90cd9208c
Ilyz <gorbacevaili891@gmail.com>
2022-11-19 18:04:13 +0300

    added in GenerationMode.java property generation mode: created TicketsGeneration impl 3 4

---
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/impl/generatway/impl/TicketsGeneratorWayImpl3.java
@@ -0,0 +1,21 @@
+package bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl;
+
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.GenerationProperty;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Question2;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Ticket;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.TicketsGeneratorWay;
+import bntu.fitr.gorbachev.ticketsgenerator.main.exceptions.GenerationConditionException;
+
+import java.util.List;
+
+public class TicketsGeneratorWayImpl3 implements TicketsGeneratorWay<Question2, Ticket<Question2>> {
+    @Override
+    public void conditionGeneration(List<Question2> questions, GenerationProperty property) throws GenerationConditionException {
+
+    }
+
+    @Override
+    public List<Ticket<Question2>> generate(Ticket<Question2> templateTicket, List<Question2> questions, GenerationProperty property) {
+        return null;
+    }
+}
--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/panels/tools/GenerationMode.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/panels/tools/GenerationMode.java
@@ -1,18 +1,37 @@
 package bntu.fitr.gorbachev.ticketsgenerator.main.panels.tools;
 
 
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Question2;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Ticket;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.TicketsGeneratorWay;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl.TicketsGeneratorWayImpl1;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl.TicketsGeneratorWayImpl2;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl.TicketsGeneratorWayImpl3;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl.TicketsGeneratorWayImpl4;
+
 /**
  * Mode enum will define generation tickets algorithm.
  */
 public enum GenerationMode {
-    MODE_1("Группировать по темам"),
-    MODE_2("Группировать по сложности"),
-    MODE_3("Рандомно"),
-    MODE_4("Группировать по темам и сложности");
+    MODE_1("Группировать по темам", TicketsGeneratorWayImpl1.class),
+    MODE_2("Группировать по сложности", TicketsGeneratorWayImpl2.class),
+    MODE_3("Рандомно", TicketsGeneratorWayImpl3.class),
+    MODE_4("Последовательно", TicketsGeneratorWayImpl4.class);
     String name;
 
-    GenerationMode(String name) {
+    Class<? extends TicketsGeneratorWay<Question2, Ticket<Question2>>> generateWay;
+
+    GenerationMode(String name, Class<? extends TicketsGeneratorWay<Question2, Ticket<Question2>>> generatorWay) {
+        this.generateWay = generatorWay;
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Class<? extends TicketsGeneratorWay<Question2, Ticket<Question2>>> getGenerateWay() {
+        return generateWay;
     }
 
     @Override
