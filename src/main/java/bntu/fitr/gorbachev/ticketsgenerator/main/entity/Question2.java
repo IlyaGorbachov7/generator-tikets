4a366e07530cf4d0303bd598929a1e72010030fa
Ilyz <gorbacevaili891@gmail.com>
2022-11-23 15:57:29 +0300

    Question2.java: added property filed: UUID

--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/Question2.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/Question2.java
@@ -1,10 +1,22 @@
 package bntu.fitr.gorbachev.ticketsgenerator.main.entity;
 
+import java.util.UUID;
+
 public class Question2 extends QuestionExt {
+    private final UUID id;
+
+    {
+        id = UUID.randomUUID();
+    }
+
     public Question2() {
     }
 
     public Question2(String section, int level, int repeat, int countRepeat) {
         super(section, level, repeat);
+    }
+
+    public UUID getId() {
+        return id;
     }
 }
