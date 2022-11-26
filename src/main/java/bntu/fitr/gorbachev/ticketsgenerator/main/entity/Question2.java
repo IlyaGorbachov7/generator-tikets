357781ee1fd001a5343b6553f630afaff6827d5c
Ilyz <gorbacevaili891@gmail.com>
2022-11-26 20:53:57 +0300

    Question2.java override method equals and hashCode

--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/Question2.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/Question2.java
@@ -1,5 +1,6 @@
 package bntu.fitr.gorbachev.ticketsgenerator.main.entity;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Question2 extends QuestionExt {
@@ -23,5 +24,24 @@ public class Question2 extends QuestionExt {
     @Override
     public String toString() {
         return getListParagraphs().get(0).getText();
+    }
+
+    /**
+     * Return {@code true} if {@code id} == {@code o.id}
+     * This method don't compare fields super class.
+     * <p>
+     * I'm Interested in the property {@link #id}, which is unique for each instance object
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question2 question2 = (Question2) o;
+        return Objects.equals(id, question2.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
