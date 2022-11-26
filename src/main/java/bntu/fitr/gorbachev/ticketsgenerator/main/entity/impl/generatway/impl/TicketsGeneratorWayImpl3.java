da743bcadd20f56dce7225ecb5643024e534e072
Ilyz <gorbacevaili891@gmail.com>
2022-11-26 20:27:03 +0300

    TicketsGeneratorWayImpl3.java I done random choose question from one of list questions

--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/impl/generatway/impl/TicketsGeneratorWayImpl3.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/entity/impl/generatway/impl/TicketsGeneratorWayImpl3.java
@@ -2,30 +2,31 @@ package bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.impl;
 
 import bntu.fitr.gorbachev.ticketsgenerator.main.entity.GenerationProperty;
 import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Question2;
+import bntu.fitr.gorbachev.ticketsgenerator.main.entity.QuestionExt;
 import bntu.fitr.gorbachev.ticketsgenerator.main.entity.Ticket;
 import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.GenerationPropertyImpl;
-import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.TicketsGeneratorWay;
 import bntu.fitr.gorbachev.ticketsgenerator.main.entity.impl.generatway.WrapperList;
 import bntu.fitr.gorbachev.ticketsgenerator.main.exceptions.GenerationConditionException;
 import bntu.fitr.gorbachev.ticketsgenerator.main.exceptions.NumberQuestionsRequireException;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class TicketsGeneratorWayImpl3 implements TicketsGeneratorWay<Question2, Ticket<Question2>> {
+public class TicketsGeneratorWayImpl3 extends TicketsGeneratorWayImpl2 {
     protected GenerationPropertyImpl prop;
     protected List<Integer> rangeQuest;
     protected WrapperList<Question2> wrapListQuest;
     protected WrapperList<Question2> wrapListRepeatQuest;
+    protected WrapperList<Question2> bufferQuestCurrentTickets;
 
     protected void initFields(List<Question2> questions, GenerationProperty property) {
         prop = (GenerationPropertyImpl) property;
-        rangeQuest = IntStream.rangeClosed(1, prop.getQuantityQTickets()).boxed().collect(Collectors.toList());
+        rangeQuest = IntStream.range(0, prop.getQuantityQTickets()).boxed().collect(Collectors.toList());
         wrapListQuest = WrapperList.of(questions);
         wrapListRepeatQuest = WrapperList.of(questions.stream().filter(q -> q.getRepeat() > 0).toList());
+        bufferQuestCurrentTickets = WrapperList.of();
     }
 
     @Override
@@ -76,33 +77,97 @@ public class TicketsGeneratorWayImpl3 implements TicketsGeneratorWay<Question2, 
     }
 
     @Override
-    public List<Ticket<Question2>> generate(Ticket<Question2> templateTicket, List<Question2> questions, GenerationProperty property) {
-        if (prop.isFlagRandomOrderReading()) {
-            Collections.shuffle(wrapListQuest.getList());
-            Collections.shuffle(wrapListRepeatQuest.getList());
-        }
-//        int possibleQuantityTickets = wrapListQuest.size()/
-
-//        int minQuantityTickets = Math.min();
-
-        return null;
+    protected int getMinQuantityPossibleTickets() {
+        int possibleQuantityTickets = wrapListQuest.size() / prop.getQuantityQTickets();
+        return Math.min(possibleQuantityTickets, prop.getQuantityTickets());
     }
 
-    private void generateTicketsWithNumber(final int minQuantityTickets, final List<Ticket<Question2>> listTickets,
-                                           Ticket<Question2> tmpT) {
+    @Override
+    protected void generateTicketsWithMinNumber(int minQuantityTickets, List<Ticket<Question2>> listTickets, Ticket<Question2> tmpT) {
         tmpT.clearQuestions();
+        IntStream.range(0, minQuantityTickets)
+                .forEach(i -> {
+                    Ticket<Question2> ticket = tmpT.clone();
+                    rangeQuest.forEach(indexQ -> ticket.add(wrapListQuest.next()));
+                    if (prop.isFlagRandomOrderQuestInTicket()) Collections.shuffle(ticket.getQuestions());
+                    listTickets.add(ticket);
+                });
+    }
 
-        for (int i = 0; i < minQuantityTickets; i++) {
-            Ticket<Question2> ticket = tmpT.clone();
-            if (prop.isFlagRandomOrderQuestInTicket()) Collections.shuffle(rangeQuest);
 
-            tmpT.setQuestions(new ArrayList<>(rangeQuest.size()));
-            for (var level :
-                    rangeQuest) {
+    @Override
+    public void generateTicketsWithRemainingNumber(List<Ticket<Question2>> listTickets) {
+        int remainingQuantity = prop.getQuantityTickets() - listTickets.size();
+        Ticket<Question2> tmpT = listTickets.get(0);
+        IntStream.range(0, remainingQuantity)
+                .mapToObj(i -> tmpT.clone())
+                .peek(this::changeQuestionsTicket)
+                .forEach(listTickets::add);
+    }
 
+    @Override
+    public void changeQuestionsTicket(Ticket<Question2> ticket) {
+        List<Question2> quests = ticket.getQuestions();
+        bufferQuestCurrentTickets.clear();
+
+        for (var index : rangeQuest) {
+            Question2 q;
+            if (wrapListQuest.hasNext()) {
+                q = tryReplaceThisQuest(null, null);
+            } else {
+                q = giveRepeatedQuest(-1);
+
+                if (q == null && !prop.isUnique()) {
+                    q = tryReplaceThisQuest(null, null);
+                } else {
+                    throw new RuntimeException("Compose unique questions non possible");
+                }
+            }
+            quests.set(index, q);
+            bufferQuestCurrentTickets.add(q);
+        }
+
+        if (prop.isFlagRandomOrderQuestInTicket()) Collections.shuffle(quests);
+    }
+
+    @Override
+    protected Question2 tryReplaceThisQuest(Question2 nullQiest, WrapperList<Question2> nullWrapperList) {
+        if (wrapListQuest.isEmpty()) throw new RuntimeException("list is empty, by try replace question");
+
+        if (!wrapListQuest.hasNext()) {
+            Collections.shuffle(wrapListQuest.getList());
+            wrapListQuest.resetCurIndex();
+        }
+        while (bufferQuestCurrentTickets.contains(wrapListQuest.current())) {
+            wrapListQuest.next();
+        }
+        return wrapListQuest.next();
+    }
+
+    @Override
+    protected Question2 giveRepeatedQuest(int level) {
+        if (wrapListRepeatQuest.isEmpty()) {
+            return null;
+        }
+        if (!wrapListRepeatQuest.hasNext()) {
+            wrapListRepeatQuest.resetCurIndex();
+        }
+
+        while (bufferQuestCurrentTickets.contains(wrapListRepeatQuest.current())) {
+            wrapListRepeatQuest.next();
+            if (!wrapListRepeatQuest.hasNext()) {
+                return null;
             }
         }
 
+        Question2 q = wrapListRepeatQuest.current();
+        q.setRepeat(q.getRepeat() - 1);
 
+        if (q.getRepeat() == 0) {
+            wrapListRepeatQuest.remove(); // removed quest with prop:repeat == 0
+        } else {
+            wrapListRepeatQuest.next();
+        }
+        return q;
     }
 }
