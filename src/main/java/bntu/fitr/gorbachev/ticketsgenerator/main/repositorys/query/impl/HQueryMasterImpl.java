50035946ed3c5bbce540a77fb9c618e040696005
Ilyz <gorbacevaili891@gmail.com>
2023-09-03 01:14:28 +0300

    Minor refactoring code

--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/repositorys/query/impl/HQueryMasterImpl.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/repositorys/query/impl/HQueryMasterImpl.java
@@ -22,40 +22,24 @@ public class HQueryMasterImpl<T> extends HQueryMaster<T> {
     @SuppressWarnings("unchecked")
     public <R> List<T> executeQuery(String queryStr, Class<R> resultClass,
                                     Map.Entry<String, Object>... params) throws DAOException {
-        Session session = getSession();
-        boolean isActiveTransactionEarly = isActiveTransaction(session);
-        try {
-            beginTransaction(isActiveTransactionEarly, session);
-            Query<?> query = session.createQuery(queryStr, resultClass);
+        return wrapTransactionalResultList(() -> {
+            Query<?> query = getSession().createQuery(queryStr, resultClass);
             setStatementParams((Query<T>) query, params);
-            @SuppressWarnings("unchecked")
-            List<T> resultList = (List<T>) query.getResultList();
-            commitTransaction(isActiveTransactionEarly, session);
-            return resultList;
-        } catch (PersistenceException e) {
-            rollbackTransaction(isActiveTransactionEarly, session);
-            throw new DAOException(e);
-        }
+            return (List<T>) query.getResultList();
+        });
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public <R> Optional<T> executeSingleEntityQuery(String queryStr, Class<R> resultClass,
                                                     Map.Entry<String, Object>... params) throws DAOException {
-        Session session = getSession();
-        boolean isActiveTransactionEarly = isActiveTransaction(session);
-        try {
-            beginTransaction(isActiveTransactionEarly, session);
-            Query<?> query = session.createQuery(queryStr, resultClass);
+        return wrapTransactionalEntitySingle(() -> {
+            Query<?> query = getSession().createQuery(queryStr, resultClass);
             setStatementParams((Query<T>) query, params);
             @SuppressWarnings("unchecked")
             T entity = (T) query.getSingleResultOrNull();
-            commitTransaction(isActiveTransactionEarly, session);
             return Optional.ofNullable(entity);
-        } catch (PersistenceException e) {
-            rollbackTransaction(isActiveTransactionEarly, session);
-            throw new DAOException(e);
-        }
+        });
     }
 
     @Override
