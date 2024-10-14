13c1b0ea9377b8a049f2cf6dc8f87854a56f3e0c
Ilyz <gorbacevaili891@gmail.com>
2024-10-14 03:22:40 +0300

    fix

--- src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/util/thememanag/ThemeAppConfiguration.java
+++ src/main/java/bntu/fitr/gorbachev/ticketsgenerator/main/util/thememanag/ThemeAppConfiguration.java
@@ -1,4 +1,9 @@
 package bntu.fitr.gorbachev.ticketsgenerator.main.util.thememanag;
 
+import java.io.IOException;
+
 public class ThemeAppConfiguration {
+    public ThemeAppConfiguration() throws IOException {
+        AppThemeManager.run();
+    }
 }
